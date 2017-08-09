fbe3057c073178298f63a62ff4160e5c8c532a15
Fangrui Song <i@maskray.me>
2017-08-09 01:11:01 -0700

    Add extend

--- clemency/r2clemency/io/io_9bit.c
+++ clemency/r2clemency/io/io_9bit.c
@@ -1,14 +1,16 @@
 /* radare - LGPL - Copyright 2017 - pancake */
 #include <errno.h>
 #include <sys/mman.h>
+#include <r_core.h>
 #include <r_io.h>
 #include <r_lib.h>
 
 typedef struct r_io_mmo_t {
 	char *filename;
+	ut16 *buf;
+	ut64 size;
 	int flags;
 	int mode;
-	RBuffer *buf;
 	bool dirty;
 } RIOMMapFileObj;
 
@@ -26,31 +28,29 @@ static int __close(RIODesc *fd) {
 	if (!fd || !fd->data) return -1;
 	RIOMMapFileObj *mmo = fd->data;
 	if (mmo->dirty) {
-		RBuffer *b = mmo->buf;
-		ut64 size = r_buf_size (b), x = 0, l = 0, n_buf = 0, i;
+		ut64 x = 0, l = 0, n_buf = 0, i;
 		bool ok = true;
-		for (i = 0; i < size; i += 2) {
-			if (b->buf[i + 1] > 1) {
+		for (i = 0; i < mmo->size; i++) {
+			if (mmo->buf[i] >= 512) {
 				ok = false;
-				eprintf ("io_9bit: 16-bit at offset %"PFMT64x" exceeds 512", i / 2);
+				eprintf ("io_9bit: 16-bit at offset %"PFMT64x" >= 512", i);
 				break;
 			}
-			x = x << 9 | b->buf[i + 1] << 8 | b->buf[i];
+			x = x << 9 | mmo->buf[i];
 			l += 9;
 			while (l >= 8) {
 				l -= 8;
-				b->buf[n_buf++] = x >> l;
+				mmo->buf[n_buf++] = x >> l;
 				x &= (1LL << l) - 1;
 			}
 		}
 		if (ok) {
 			ssize_t t;
-			if (l) {
-				b->buf[n_buf++] = x << 8-l;
-			}
+			if (l)
+				mmo->buf[n_buf++] = x << 8-l;
 			if (lseek (fd->fd, 0, SEEK_SET) == -1) {
 				eprintf ("io_9bit lseek: %s\n", strerror (errno));
-			} else if ((t = write (fd->fd, b->buf, n_buf)) != n_buf) {
+			} else if ((t = write (fd->fd, mmo->buf, n_buf)) != n_buf) {
 				eprintf ("io_9bit: written %zd bytes, %s\n", t, strerror (errno));
 			} else if (ftruncate (fd->fd, n_buf) == -1) {
 				eprintf ("io_9bit ftruncate: %s\n", strerror (errno));
@@ -58,22 +58,32 @@ static int __close(RIODesc *fd) {
 		}
 	}
 	close (fd->fd);
-	r_buf_free (mmo->buf);
+	free (mmo->buf);
 	free (mmo->filename);
 	free (mmo);
 	fd->data = NULL;
 	return 0;
 }
 
+static int extend(RIO *io, RIODesc *fd, ut64 extend) {
+	RIOMMapFileObj *mmo = fd->data;
+	ut64 addr = io->off, size = mmo->size;
+	mmo->buf = realloc (mmo->buf, (size + extend) * 2);
+	if (!mmo->buf) return -1;
+	memmove (&mmo->buf[addr + extend], &mmo->buf[addr], (size - addr) * 2);
+	memset (&mmo->buf[addr], 0, extend * 2);
+	return extend;
+}
+
 static ut64 __lseek(RIO *io, RIODesc *fd, ut64 offset, int whence) {
 	RIOMMapFileObj *mmo = fd->data;
 	ut64 seek_val;
 	switch (whence) {
-	case SEEK_SET: seek_val = R_MIN (mmo->buf->length / 2, offset); break;
-	case SEEK_CUR: seek_val = R_MIN (mmo->buf->length / 2, io->off + offset); break;
-	case SEEK_END: seek_val = mmo->buf->length / 2; break;
+	case SEEK_SET: seek_val = R_MIN (mmo->size, offset); break;
+	case SEEK_CUR: seek_val = R_MIN (mmo->size, io->off + offset); break;
+	case SEEK_END: seek_val = mmo->size; break;
 	}
-	return mmo->buf->cur = io->off = seek_val;
+	return io->off = seek_val;
 }
 
 static RIODesc *__open(RIO *io, const char *filename, int flags, int mode) {
@@ -83,7 +93,7 @@ static RIODesc *__open(RIO *io, const char *filename, int flags, int mode) {
 	if (fd == -1) return NULL;
 	off_t len = lseek (fd, 0, SEEK_END);
 	if (len < 0) goto err_lseek;
-	ut8 *buf = mmap (NULL, len, PROT_READ, MAP_SHARED, fd, 0);
+	ut8 *buf = len > 0 ? mmap (NULL, len, PROT_READ, MAP_SHARED, fd, 0) : NULL;
 	if (buf == (ut8 *)-1) goto err_lseek;
 
 	RIOMMapFileObj *mmo = R_NEW (RIOMMapFileObj);
@@ -93,7 +103,8 @@ static RIODesc *__open(RIO *io, const char *filename, int flags, int mode) {
 	mmo->flags = flags;
 	mmo->mode = mode;
 	if (len >= 2) {
-		mmo->buf = r_buf_new_empty (len * 8 / 9 * 2);
+		mmo->size = len * 8 / 9;
+		mmo->buf = malloc (mmo->size * 2);
 		if (!mmo->buf) goto err_mmo_buf;
 	} else
 		mmo->buf = NULL;
@@ -105,11 +116,11 @@ static RIODesc *__open(RIO *io, const char *filename, int flags, int mode) {
 		l += 8;
 		if (l >= 9) {
 			l -= 9;
-			mmo->buf->buf[n_buf++] = x >> l & 255;
-			mmo->buf->buf[n_buf++] = x >> l >> 8;
+			mmo->buf[n_buf++] = x >> l;
 			x &= (1L << l) - 1;
 		}
 	}
+	r_config_set_i (((RCore *)io->user)->config, "asm.addrbytes", 2);
 	return r_io_desc_new (&r_io_plugin_9bit, fd, mmo->filename, flags, mode, mmo);
 
  err_mmo_buf:
@@ -117,22 +128,30 @@ static RIODesc *__open(RIO *io, const char *filename, int flags, int mode) {
  err_strdup:
 	free (mmo);
  err_mmo:
-	munmap (buf, len);
+	if (buf)
+		munmap (buf, len);
  err_lseek:
 	r_sandbox_close (fd);
 	return NULL;
 }
 
-static int __read(RIO *io, RIODesc *fd, ut8 *buf, int count) {
+static int __read(RIO *io, RIODesc *fd, ut8 *buf, int len) {
 	RIOMMapFileObj *mmo = fd->data;
-	return r_buf_read_at (mmo->buf, io->off * 2, buf, count);
+	if (len <= (mmo->size - io->off) * 2)
+		memcpy (buf, &mmo->buf[io->off], len);
+	else {
+		memcpy (buf, &mmo->buf[io->off], (mmo->size - io->off) * 2);
+		memset (&buf[(mmo->size - io->off) * 2], 0xff, io->off * 2 + len - mmo->size * 2);
+	}
+	return len;
 }
 
 static int __write(RIO *io, RIODesc *fd, const ut8 *buf, int len) {
 	RIOMMapFileObj *mmo = fd->data;
-	if (io->off * 2 + len > r_buf_size (mmo->buf)) return -1;
+	if (io->off * 2 + len > mmo->size * 2) return -1;
 	mmo->dirty = true;
-	return r_buf_write_at (mmo->buf, io->off * 2, buf, len);
+	memcpy (mmo->buf + io->off * 2, buf, len);
+	return len;
 }
 
 RIOPlugin r_io_plugin_9bit = {
@@ -141,6 +160,7 @@ RIOPlugin r_io_plugin_9bit = {
 	.license = "LGPL3",
 	.check = check,
 	.close = __close,
+	.extend = extend,
 	.lseek = __lseek,
 	.open = __open,
 	.read = __read,
